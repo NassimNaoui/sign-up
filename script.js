2f96cf6a1c65515da2b35d6e5ba94ed60942e00c
Nassanalytics <nassim.naoui@gmail.com>
2024-02-24 18:11:15 +0100

    Ajout du header

---
+++ script.js
@@ -0,0 +1,21 @@
+// Sélectionner l'élément par classe
+var maDiv = document.querySelector('.sidebar');
+
+// Vérifier si l'élément a été trouvé
+if (maDiv) {
+  // Récupérer les dimensions de la div par rapport à la fenêtre
+  var dimensions = maDiv.getBoundingClientRect();
+
+  // Récupérer la hauteur en vh
+  var hauteurEnVh = (dimensions.height / window.innerHeight) * 100;
+
+  // Calculer la valeur pour margin-top
+  var marginAuto = `calc((100vh / 3) - ${hauteurEnVh}vh)`;
+
+  // Appliquer la valeur à la propriété margin-top
+  maDiv.style.marginTop = marginAuto;
+
+  console.log('La hauteur de maDiv est de ' + hauteurEnVh + 'vh');
+} else {
+  console.error('Aucun élément avec la classe "sidebar" trouvé.');
+}
